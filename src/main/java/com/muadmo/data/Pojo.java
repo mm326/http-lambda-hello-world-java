12b1af6ebf3e9d44f9176eaad879dab351dbaa6f
mm326 <muadhabdullahi@gmail.com>
2020-07-01 16:09:50 +0100

    Use http api-gatewat as invokation

---
+++ src/main/java/com/muadmo/data/Pojo.java
@@ -0,0 +1,15 @@
+package com.muadmo.data;
+
+public class Pojo {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
