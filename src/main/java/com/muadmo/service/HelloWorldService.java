cc1bb632e595146aba35ae1a4f0006820ed5daf5
mm326 <muadhabdullahi@gmail.com>
2020-07-07 19:30:09 +0100

    Add lambda to get all names

--- src/main/java/com/muadmo/service/HelloWorldService.java
+++ src/main/java/com/muadmo/service/HelloWorldService.java
@@ -1,6 +1,8 @@
 package com.muadmo.service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -13,6 +15,12 @@ import com.muadmo.data.Pojo;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+<<<<<<< Updated upstream
+=======
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+>>>>>>> Stashed changes
 
 public class HelloWorldService {
 
@@ -24,22 +32,60 @@ public class HelloWorldService {
         this.dynamoDbClient = dynamoDBClient;
     }
 
+<<<<<<< Updated upstream
     public APIGatewayProxyResponseEvent inputToUpperCase(APIGatewayProxyRequestEvent input) throws JsonMappingException, JsonProcessingException {
         Pojo body = objectMapper.readValue(input.getBody(), Pojo.class);
         String upperCaseName = body.getName().toUpperCase();
         body.setName(upperCaseName);
         putItemInTable(upperCaseName);
         String responseBody = objectMapper.writeValueAsString(body);
+=======
+    public APIGatewayProxyResponseEvent inputToUpperCase(APIGatewayProxyRequestEvent request)
+            throws JsonMappingException, JsonProcessingException {
+        JSONObject body = new JSONObject(request.getBody());
+        String name = body.getString("name");
+        String upperCaseName = name.toUpperCase();
+        putItemInTable(name);
+        String responseBody = "{\"name\":\"" + upperCaseName + "\"}";
+>>>>>>> Stashed changes
         return new APIGatewayProxyResponseEvent().withBody(responseBody).withStatusCode(200);
     }
 
     public void putItemInTable(String name) {
         Map<String, AttributeValue> item = Map.of("nameId", AttributeValue.builder().s(name).build());
-        PutItemRequest putItemRequest = PutItemRequest.builder()
-                .tableName(TABLE_NAME)
-                .conditionExpression("attribute_not_exists(nameId)")
-                .item(item)
-                .build();
+        PutItemRequest putItemRequest = PutItemRequest.builder().tableName(TABLE_NAME)
+                .conditionExpression("attribute_not_exists(nameId)").item(item).build();
         dynamoDbClient.putItem(putItemRequest);
     }
+<<<<<<< Updated upstream
+=======
+
+    public APIGatewayProxyResponseEvent getNameFromDatabase(APIGatewayProxyRequestEvent request) {
+        String name = request.getQueryStringParameters().getOrDefault("id", null);
+        QueryRequest queryRequest = QueryRequest.builder().keyConditionExpression("nameId = :" + name)
+                .tableName(TABLE_NAME)
+                .expressionAttributeValues(Map.of(":" + name, AttributeValue.builder().s(name).build())).build();
+        Map<String, AttributeValue> item = dynamoDbClient.query(queryRequest).items().get(0);
+        String nameValue = item.get("nameId").s().toUpperCase();
+        String body = "{\"name\":\"" + nameValue + "\"}";
+        return new APIGatewayProxyResponseEvent().withBody(body).withStatusCode(200);
+    }
+
+    public APIGatewayProxyResponseEvent getAllNamesFromDatabase(APIGatewayProxyRequestEvent request) {
+        ScanRequest scanRequest = ScanRequest.builder().tableName(TABLE_NAME).projectionExpression("nameId").build();
+        ScanResponse scanResponse = dynamoDbClient.scan(scanRequest);
+        List<String> items = scanResponse.items().stream()
+                .map(s -> s.get("nameId").s())
+                .collect(Collectors.toList());
+              
+        List<JSONObject> body = items.stream()
+                .map(n -> makeJson(n))
+                .collect(Collectors.toList());
+        return new APIGatewayProxyResponseEvent().withBody(body.toString()).withStatusCode(200);
+    }
+
+    private JSONObject makeJson(String n) {
+        return new JSONObject().accumulate("name", n);
+    }
+>>>>>>> Stashed changes
 }
--- src/test/java/com/muadmo/service/HelloWorldServiceTest.java
+++ src/test/java/com/muadmo/service/HelloWorldServiceTest.java
@@ -21,6 +21,13 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+<<<<<<< Updated upstream
+=======
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+>>>>>>> Stashed changes
 
 @ExtendWith(MockitoExtension.class)
 public class HelloWorldServiceTest {
@@ -57,5 +64,37 @@ public class HelloWorldServiceTest {
         verify(dynamoDbClient).putItem(argumentCaptor.capture());
         assertEquals(expectedItem, argumentCaptor.getValue().item());
     }
+<<<<<<< Updated upstream
+=======
+    
+    @Test
+    @SuppressWarnings("unchecked")
+    void shouldGetNameFromDatabase() {
+            String expectedJson = "{\"name\":\"MUAD\"}";
+            APIGatewayProxyResponseEvent expectedResponse = new APIGatewayProxyResponseEvent().withBody(expectedJson).withStatusCode(200);
+            APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent().withQueryStringParameters(Map.of("id", "muad"));
+            Map<String, AttributeValue> item = Map.of("nameId", AttributeValue.builder().s("MUAD").build());
+            
+            QueryResponse response = QueryResponse.builder().count(1).items(item).build();
+            when(dynamoDbClient.query(any(QueryRequest.class))).thenReturn(response);
+            APIGatewayProxyResponseEvent actualResponse = handler.getNameFromDatabase(input);
+            assertEquals(expectedResponse, actualResponse);
+    }
+    
+    @Test
+    @SuppressWarnings("unchecked")
+    void shouldGetAllNamesFromDatabase() {
+            String expectedJson = "[{\"name\":\"muad\"}, {\"name\":\"alex\"}]";
+            APIGatewayProxyResponseEvent expectedResponse = new APIGatewayProxyResponseEvent().withBody(expectedJson).withStatusCode(200);
+            APIGatewayProxyRequestEvent input = new APIGatewayProxyRequestEvent().withPath("/names");
+            Map<String, AttributeValue> item1 = Map.of("nameId", AttributeValue.builder().s("muad").build());
+            Map<String, AttributeValue> item2 = Map.of("nameId", AttributeValue.builder().s("alex").build());
+            
+            ScanResponse scanResponse = ScanResponse.builder().items(item1, item2).build();
+            when(dynamoDbClient.scan(any(ScanRequest.class))).thenReturn(scanResponse);
+            APIGatewayProxyResponseEvent actualResponse = handler.getAllNamesFromDatabase(input);
+            assertEquals(expectedResponse, actualResponse);
+    }
+>>>>>>> Stashed changes
 
 }
